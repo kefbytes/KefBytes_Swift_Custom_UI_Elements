35d4e8a7fc76672f5de29979e8fd4058e664ccf2
Kent <kent@kefbytes.com>
2015-12-15 14:05:31 -0500

    Initial Commit

---
+++ KefCustomUIElements/KefButton.swift
@@ -0,0 +1,88 @@
+//
+//  KefButton.swift
+//  KefCustomUIElements
+//
+//  Created by Franks, Kent on 12/15/15.
+//  Copyright © 2015 Franks, Kent. All rights reserved.
+//
+
+
+/*
+Button not yet functional
+*/
+
+import UIKit
+
+@IBDesignable public class KefButton: UIControl {
+    
+    private let button: UIButton = UIButton(type: UIButtonType.Custom)
+    
+    // MARK: - IB Editable Properties
+    @IBInspectable public var title: String = "" {
+        didSet { setNeedsLayout() }
+    }
+
+    @IBInspectable public var titleColor: UIColor = UIColor.blackColor() {
+        didSet { setNeedsLayout() }
+    }
+
+    @IBInspectable public var buttonColor: UIColor = UIColor.clearColor() {
+        didSet { setNeedsLayout() }
+    }
+
+    @IBInspectable public var borderColor: UIColor = UIColor.clearColor() {
+        didSet { setNeedsLayout() }
+    }
+
+    @IBInspectable public var borderWidth: CGFloat = 0.0 {
+        didSet { setNeedsLayout() }
+    }
+
+    @IBInspectable public var cornerRadius: CGFloat = 0.0 {
+        didSet { setNeedsLayout() }
+    }
+
+    // MARK: - Initializers
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialiseView()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialiseView()
+    }
+    
+    // MARK: - Setup Views
+    private func initialiseView() {
+        self.addSubview(button)
+        button.addTarget(self, action: Selector("buttonTouchUpInside:event:"), forControlEvents: UIControlEvents.TouchUpInside)
+        button.addTarget(self, action: Selector("touchDown:event:"), forControlEvents: UIControlEvents.TouchDown)
+        setNeedsLayout()
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        button.frame = CGRectMake(0, 0, self.frame.width, self.frame.height)
+        button.setTitleColor(titleColor, forState: UIControlState.Normal)
+        button.setTitle(title, forState: UIControlState.Normal)
+        button.backgroundColor = buttonColor
+        button.layer.borderColor = borderColor.CGColor
+        button.layer.borderWidth = borderWidth
+        button.layer.cornerRadius = cornerRadius
+    }
+    
+    // MARK: - Action Events
+    public func touchDown(control: UIControl,event: AnyObject) {
+        UIView.animateWithDuration(0.1) { () -> Void in
+            let newtransform = CGAffineTransformMakeScale(0.985, 0.985)
+            self.transform = newtransform
+        }
+    }
+    
+    public func buttonTouchUpInside(control: UIControl,event: AnyObject) {
+        sendActionsForControlEvents(UIControlEvents.TouchUpInside)
+    }
+
+    
+}
---
+++ KefCustomUIElements/KefTextField.swift
@@ -0,0 +1,81 @@
+//
+//  KefTextField.swift
+//  KefCustomUIElements
+//
+//  Created by Franks, Kent on 12/15/15.
+//  Copyright © 2015 Franks, Kent. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class KefTextField: UIControl {
+    
+    private let textField: UITextField = UITextField(frame: CGRectMake(0, 0, 305, 55))
+    
+    // MARK: - IB Setable Properties
+    @IBInspectable var cornerRadius: CGFloat = 28.0 {
+        didSet { setNeedsLayout() }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 1.0 {
+        didSet { setNeedsLayout() }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.lightGrayColor() {
+        didSet { setNeedsLayout() }
+    }
+    
+    @IBInspectable var placeholder: String = "" {
+        didSet { setNeedsLayout() }
+    }
+    
+    @IBInspectable var fontSize: CGFloat = 14 {
+        didSet { setNeedsLayout() }
+    }
+    
+    @IBInspectable var inset: CGFloat = 25 {
+        didSet { setNeedsLayout() }
+    }
+    
+    
+    // MARK: - Initializers
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialiseView()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialiseView()
+    }
+    
+    // MARK: - Creating the views
+    private func initialiseView() {
+        self.addSubview(textField)
+        setNeedsLayout()
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        textField.layer.frame = CGRectMake(0, 0, self.frame.width, self.frame.height)
+        
+        textField.layer.cornerRadius = cornerRadius
+        textField.layer.borderWidth = borderWidth
+        textField.layer.borderColor = borderColor.CGColor
+        
+        let attributes = [
+            NSForegroundColorAttributeName: UIColor.lightGrayColor(),
+            NSFontAttributeName : UIFont(name: "HelveticaNeue", size: fontSize)!
+        ]
+        
+        textField.attributedPlaceholder = NSAttributedString(string: placeholder, attributes:attributes)
+        
+        let paddingView = UIView(frame: CGRectMake(0, 0, inset, self.textField.frame.height))
+        textField.leftView = paddingView
+        textField.leftViewMode = UITextFieldViewMode.Always
+        
+        self.addSubview(textField)
+    }
+    
+}
+
